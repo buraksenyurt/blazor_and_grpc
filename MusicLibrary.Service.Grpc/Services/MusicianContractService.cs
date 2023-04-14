76a7fd10276dad827b06b2d33704b36ff75b1f71
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-14 18:51:55 +0300

    Added grpc service project and

---
+++ MusicLibrary.Service.Grpc/Services/MusicianContractService.cs
@@ -0,0 +1,10 @@
+using Grpc.Core;
+using MusicLibrary.Service.Grpc.Protos;
+
+namespace MusicLibrary.Service.Grpc.Services;
+
+public class MusicianContractService
+    : MusicianContract.MusicianContractBase
+{
+
+}
