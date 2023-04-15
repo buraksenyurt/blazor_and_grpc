b12c8a77eac2a9ef0a3d596fd95f777b64001d1b
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-15 23:55:02 +0300

    Added CORS support

--- MusicLibrary.Service.Grpc/Program.cs
+++ MusicLibrary.Service.Grpc/Program.cs
@@ -1,18 +1,30 @@
+using Microsoft.Net.Http.Headers;
+using MusicLibrary.Data;
+using MusicLibrary.Service;
 using MusicLibrary.Service.Grpc;
 using MusicLibrary.Service.Grpc.Contracts;
-using MusicLibrary.Shared.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddGrpc();
-builder.Services.AddAutoMapper(typeof(GrpcMapper));
+builder.Services.AddCors(setupAction =>
+{
+    setupAction.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod().WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+    });
+});
+builder.Services.AddTransient<AlbumService>();
+builder.Services.AddTransient<MusicianService>();
+builder.Services.AddDataContext(configuration);
 
 var app = builder.Build();
-
 app.UseGrpcWeb();
+app.UseCors();
 app.MapGrpcService<MusicianContractService>().EnableGrpcWeb();
 app.MapGrpcService<AlbumContractService>().EnableGrpcWeb();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+//app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
