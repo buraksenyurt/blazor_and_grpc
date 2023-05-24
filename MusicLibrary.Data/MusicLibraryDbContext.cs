b459c8bb84d3419bee0f16f5ddf0c2ea9f5230bd
buraksenyurt <burakselimsenyurt@gmail.com>
2023-05-24 14:28:46 +0300

    Added some data for seed.

--- MusicLibrary.Data/MusicLibraryDbContext.cs
+++ MusicLibrary.Data/MusicLibraryDbContext.cs
@@ -25,18 +25,29 @@ public class MusicLibraryDbContext
         {
             m.Property(p => p.Name).IsRequired();
             m.Navigation(p => p.Albums).AutoInclude();
+            m.HasData(
+                new Musician { Id = 1, Name = "John Doe", Bio = "Londra doğumlu biz caz ustasıdır.", StarPoint = 6, Birthdate = new DateTime(1965, 12, 4) },
+                new Musician { Id = 2, Name = "Maria Keri Watson", Bio = "Çikolata renkli kadife sesin sahibi Amerikalı R&B şarkıcısıdır.", StarPoint = 8, Birthdate = new DateTime(1980, 1, 1) },
+                new Musician { Id = 3, Name = "Ben Deyvis Ya Sen", Bio = "Dublin doğumlu cult rock solistidir. Daha şimdiden sayısız albümde yer almıştır.", StarPoint = 6, Birthdate = new DateTime(1998, 12, 12) }
+            );
         });
 
         modelBuilder.Entity<Album>(a =>
         {
             a.Property(p => p.Name).IsRequired();
             a.Navigation(p => p.Musicians).AutoInclude();
+            a.HasData(
+                new Album { Id = 1, Name = "Night At The Museum II", Year = 2019, Category = Category.Rock },
+                new Album { Id = 2, Name = "Actions & Reactions", Year = 1981, Category = Category.Reggae },
+                new Album { Id = 3, Name = "Everything I Do I Do It For You", Year = 1993, Category = Category.Rock }
+            );
         });
 
         // Album ve müzisyenler arasındaki many-to-many ilişkiyi tanımlar.
         modelBuilder.Entity<AlbumMusician>(a =>
         {
             a.HasKey(p => new { p.AlbumId, p.MusicianId });
+
         });
     }
 }
